0aaa744523fff119b08e75382b0900d9969221b4
Jun <junyuplee@mines.edu>
2022-12-07 02:36:36 -0700

    adding finished skarbek code

---
+++ floorplans/skarbek.py
@@ -0,0 +1,83 @@
+import copy
+
+class ArrayTree:
+    def __init__(self, total, index):
+        self.l = [None]*total
+        self.r = [None]*total
+        self.order = index
+    def copy(self, other):
+        self.l = copy.deepcopy(other.l)
+        self.r = copy.deepcopy(other.r)
+    def print(self):
+        print('Iteration %d' % self.order)
+        n = len(self.r)
+        for i in range(0, n, 1):
+            print(self.l[i], end = ' ')
+        print()
+        for i in range(0, n, 1):
+            print(self.r[i], end = ' ')
+    
+def visitTree(bTree, n):
+    # P3 Find j
+    j = 1
+    while (bTree.l[j-1]==0):
+        bTree.r[j-1] = 0
+        bTree.l[j-1] = j+1
+        j+=1
+        if j>n-1:
+            return None
+    # P4 Find k and y
+    y = bTree.l[j-1]
+    k = 0
+    while (bTree.r[y-1]>0):
+        k = y
+        y = bTree.r[y-1]
+    # P5 Promote y
+    if k>0:
+        bTree.r[k-1] = 0
+    else:
+        bTree.l[j-1] = 0
+    
+    bTree.r[y-1] = bTree.r[j-1]
+    bTree.r[j-1] = y
+    return bTree
+
+def algorithm(n):
+    index = 1
+    if n<1:
+        bTree = ArrayTree(1,1)
+        bTree.l[0] = 0
+        bTree.r[0] = 0
+        return [bTree]
+    else:
+        bTree = ArrayTree(n,index)
+        bForest = []
+
+        while (bTree != None):
+            if (index==1): # P1 initialize
+                for k in range(0,n-1,1):
+                    bTree.l[k] = k+2
+                    bTree.r[k] = 0
+                bTree.l[n-1] = bTree.r[n-1] = 0
+                bTree.l.append(1)
+                doneTree = bTree
+            else:
+                # P2 visit binary tree
+                doneTree = visitTree(bTree, n) 
+            if (doneTree!=None):
+                bForest.append(doneTree)
+            else:
+                break
+            index+=1
+            bTree = ArrayTree(n, index)
+            bTree.copy(doneTree)
+    
+    return bForest
+
+def skarbek(n, doPrint): # n = # of internal nodes
+    ARList = algorithm(int(n))
+    if doPrint:
+        for tree in ARList:
+            tree.print()
+            print('\n')
+    return ARList
