5e29b636797566a0f30e43ebef17d31bc25372ab
Jun <junyuplee@mines.edu>
2023-02-15 09:04:00 -0700

    Adding in skewes for mazes

--- maze_generation/binaryTrees.py
+++ maze_generation/binaryTrees.py
@@ -1,11 +1,16 @@
 from random import randint
 
 class BinaryTree: # Binary Tree Algorithm
-    def __init__(self, grid):
+    def __init__(self, grid, skew):
+        skewes = {
+            'NW': ('north', 'west'),
+            'NE': ('north', 'east'),
+            'SW': ('south', 'west'),
+            'SE': ('south', 'east'),}
         for cell in grid.each_cell():
             neighbors = []
-            neighbors.append(cell.neighbors.get('north'))
-            neighbors.append(cell.neighbors.get('east'))
+            neighbors.append(cell.neighbors.get(skewes.get(skew)[0]))
+            neighbors.append(cell.neighbors.get(skewes.get(skew)[1]))
             neighbors = [i for i in neighbors if i is not None]
 
             if len(neighbors) == 2:
@@ -16,11 +21,11 @@ class BinaryTree: # Binary Tree Algorithm
                 continue
 
             neighbor = neighbors[index]
-            cell.link(neighbor)
+            cell.link(neighbor)        
 
-def generateMaze(grid):
-    BinaryTree(grid)
+def generateMaze(grid, skew):
+    skewes = ('NE', 'NW', 'SW', 'SE')
+    if skew.upper() not in skewes:
+        skew = skewes[randint(0, 3)]
+    BinaryTree(grid, skew.upper())
     return grid
-
-from objects import Grid
-print(generateMaze(Grid(24, 18)))
