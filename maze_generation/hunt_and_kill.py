b65385774241ca6ba316b932f441dd543a72824b
Jun <junyuplee@mines.edu>
2023-05-12 00:19:19 -0600

    testing hunt and kill

--- maze_generation/hunt_and_kill.py
+++ maze_generation/hunt_and_kill.py
@@ -7,24 +7,31 @@ def hunt_and_kill(grid):
     current = grid.getRandom()
 
     while current:
+        print(grid) #proofreading
         neighbors = current.getNeighbors()
 
         unvisited_neighbors = [n for n in neighbors 
-                               if not current.isLinked(n)]
+                               if not n.getLinks()]
         if unvisited_neighbors:
-            neighbor = unvisited_neighbors.pop()
+            neighbor = sample(unvisited_neighbors,1)[0]
             current.link(neighbor)
             current = neighbor
         else:
             current = None # to break out while loop if all visited
 
             for cell in grid.each_cell():
+                neighbors = cell.getNeighbors()
                 visited_neighbors = [n for n in neighbors
-                                     if cell.isLinked(n)]
+                                     if n.getLinks()]
 
                 # if visited neighbors exist and cell has no links
-                if visited_neighbors and bool(cell.getLinks):
+                if visited_neighbors and not cell.getLinks():
                     current = cell
-                    neighbor = sample(visited_neighbors,1)
+                    neighbor = sample(visited_neighbors,1)[0]
                     current.link(neighbor)
                     break
+
+from board import Grid
+grid = Grid(4,4)
+hunt_and_kill(grid)
+print("DONE")
