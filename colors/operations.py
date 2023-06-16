163989f2833e4a5343c61097d0e300ee7c6273b3
Jun <junyuplee@mines.edu>
2023-06-15 22:24:36 -0600

    clarifying a variable

--- colors/operations.py
+++ colors/operations.py
@@ -6,7 +6,6 @@ primary = {'red': (255, 0, 0), # primary colors of light
 
 def pastel(rgb: tuple) -> tuple: # returns pastel color
     rgb = list(rgb)
-    for i,c in enumerate(rgb):
-        rgb[i] = (c + 255)//2 # add white to each primary
+    for i,part in enumerate(rgb):
+        rgb[i] = (part + 255)//2 # add white to each primary
     return tuple(rgb)
-
