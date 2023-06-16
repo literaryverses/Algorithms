c4d95a33cb006e81d1952c0d4a6bc04aa59833e0
Jun <junyuplee@mines.edu>
2023-06-15 22:50:35 -0600

    adding upcoming sorting files

---
+++ sorting/quicksort.py
@@ -0,0 +1,13 @@
+def quicksort_lomuto():
+    return
+
+def quicksort_hoare():
+    return
+
+def multipivot():
+    return
+
+def multikey():
+    return
+
+    
---
+++ sorting/selectionsort.py
@@ -0,0 +1,11 @@
+def selectionsort(array):
+    return
+
+def heapsort(array):
+    return
+
+def bingosort(array):
+    return
+
+def cocktailsort(array): # double selection sort
+    return
