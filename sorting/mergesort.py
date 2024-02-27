e5e19614349522abd7c267a08ff3d4ddb0375168
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-02-26 23:47:07 -0700

    adding threeway mergesort

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -31,22 +31,76 @@ def mergesort(array):
 
 
 def three_way_mergesort(array):
+    if len(array) > 1:
+        mid1 = len(array) // 3
+        mid2 = mid1 * 2
+        left_part = array[:mid1]
+        mid_part = array[mid1:mid2]
+        right_part = array[mid2:]
+
+        three_way_mergesort(left_part)
+        three_way_mergesort(mid_part)
+        three_way_mergesort(right_part)
+
+        i = j = k = l = 0
+
+        while i < len(left_part) and j < len(mid_part) and k < len(right_part):
+            if left_part[i] < mid_part[j]:
+                if left_part[i] < right_part[k]:
+                    array[l] = left_part[i]
+                    i += 1
+                else:
+                    array[l] = right_part[k]
+                    k += 1
+            else:
+                if mid_part[j] < right_part[k]:
+                    array[l] = mid_part[j]
+                    j += 1
+                else:
+                    array[l] = right_part[k]
+                    k += 1
+            l += 1
+
+        while i < len(left_part) and j < len(mid_part):
+            if left_part[i] < mid_part[j]:
+                array[l] = left_part[i]
+                i += 1
+            else:
+                array[l] = mid_part[j]
+                j += 1
+            l += 1
+
+        while j < len(mid_part) and k < len(right_part):
+            if mid_part[j] < right_part[k]:
+                array[l] = mid_part[j]
+                j += 1
+            else:
+                array[l] = right_part[k]
+                k += 1
+            l += 1
+
+        while i < len(left_part):
+            array[l] = left_part[i]
+            i += 1
+            l += 1
+
+        while j < len(mid_part):
+            array[l] = mid_part[j]
+            j += 1
+            l += 1
+
+        while k < len(right_part):
+            array[l] = right_part[k]
+            k += 1
+            l += 1
+    return array
+
+
+def iterative_mergesort(array):  # bottom-up mergesort
     return array
 
 
 def in_place_mergesort(array):
-    return array
-
-
-def in_place_mergesort_2(array):
-    return array
-
-
-def merge_sort_with_O1_space(array):
-    return array
-
-
-def bottom_up_merge_sort(array):
     return array
 
 
@@ -56,8 +110,3 @@ def natural_merge_sort(array):
 
 def parallel_merge_sort(array):
     return array
-
-
-arr = [12, 11, 13, 5, 6, 7, 1]
-mergesort(arr)
-print("Sorted array:", arr)
