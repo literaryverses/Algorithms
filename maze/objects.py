768a6a3a749afb939a8e73b855b0e6fa50150508
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-17 22:30:31 -0700

    move path to objects

--- maze/objects.py
+++ maze/objects.py
@@ -1,4 +1,5 @@
 from random import randint, random, shuffle, choice
+from collections import Counter
 
 """
 NOTES:
@@ -499,3 +500,30 @@ class HexGrid(Grid):  # sigma maze (shape = 6)
             for x in range(3):
                 triGrid.mask(row, col + x, lvl)  # top row of hexagon
                 triGrid.mask(row + 1, col + x, lvl)  # bottom row of hexagon
+
+
+class Path:
+    def __init__(self, start):
+        self.path = [start]
+
+    def append(self, cell):  # add cell to path
+        self.path.append(cell)
+
+    def pop(self, cell=None):  # remove cell from path
+        if cell:
+            self.path.pop(cell)
+        else:
+            self.path.pop()
+
+    def getCoords(self):  # get only coordinates of path
+        for cell in self.path:
+            yield cell.coord
+
+    def cleanup(self):  # remove loops
+        repeats = [item for item, count in Counter(self.path).items() if count > 1]
+        for repeat in repeats:
+            while self.path.count(repeat) > 1:
+                i1 = self.path.index(repeat)
+                i2 = self.path.index(repeat, i1 + 1)
+                self.path = self.path[:i1] + self.path[i2:]
+        return self.path
